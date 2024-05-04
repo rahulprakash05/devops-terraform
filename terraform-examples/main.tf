80a3e531f875d8ae9ac881e8896212802c7a6894
rahulprakash05 <148333241+rahulprakash05@users.noreply.github.com>
2024-05-04 11:10:48 +0530

    Examples terraform

---
+++ terraform-examples/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example_instance" {
+    ami = ""
+    instacne_type = ""
+    subnet_id = ""
+}
---
+++ terraform-examples/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-07caf09b362be10b8"
+instance_type_value = "t2.micro"
+subnet_id_value = "subnet-00f73d71efb150304"
