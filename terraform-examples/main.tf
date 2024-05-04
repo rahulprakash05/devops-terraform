18f7ab35d11a61e278dc0f3b3243ee528205a23f
rahulprakash05 <148333241+rahulprakash05@users.noreply.github.com>
2024-05-04 11:21:20 +0530

    instance spell mistake

--- terraform-examples/main.tf
+++ terraform-examples/main.tf
@@ -1,9 +1,21 @@
+variable "ami_value" {
+    description = "Value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
+
 provider "aws" {
     region = "us-east-1"
 }
 
 resource "aws_instance" "example_instance" {
-    ami = ""
-    instacne_type = ""
-    subnet_id = ""
+    ami = "var.ami_value"
+    instance_type = "var.instance_type_value"
+    subnet_id = "var.subnet_id_value"
 }
